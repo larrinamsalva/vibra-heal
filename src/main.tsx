0d0a5b078ed2a27ed46af9f5dacb1b9c2dcb41bd
Larrina Salva <larrina.m.salva@gmail.com>
2026-07-31 18:48:14 -0700

    Mount guided breathing and session bridge

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,20 @@ import AccessibilityControls from './AccessibilityControls'
 import AnimalCalm from './AnimalCalm'
 import App from './App'
 import BackupRestore from './BackupRestore'
+import BreathingGuide from './BreathingGuide'
+import { installBreathingSessionBridge } from './breathingSessionBridge'
 import NatureMixer from './NatureMixer'
 import PwaInstall from './PwaInstall'
 import './styles.css'
+
+installBreathingSessionBridge()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AccessibilityControls />
     <App />
     <BackupRestore />
+    <BreathingGuide />
     <NatureMixer />
     <AnimalCalm />
     <PwaInstall />
