8bb42adc569b077273a3e98043b4c8811a08cf4f
Larrina Salva <larrina.m.salva@gmail.com>
2026-07-31 18:19:21 -0700

    Mount accessibility controls before the app

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import AccessibilityControls from './AccessibilityControls'
 import AnimalCalm from './AnimalCalm'
 import App from './App'
 import NatureMixer from './NatureMixer'
@@ -8,6 +9,7 @@ import './styles.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <AccessibilityControls />
     <App />
     <NatureMixer />
     <AnimalCalm />
