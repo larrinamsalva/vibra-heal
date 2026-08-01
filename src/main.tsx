6dd386add75cdb2d4f30b0b6aeac116786e7e55c
Larrina Salva <larrina.m.salva@gmail.com>
2026-07-31 17:29:27 -0700

    Mount the nature ambience mixer in VibraHeal

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import AnimalCalm from './AnimalCalm'
 import App from './App'
+import NatureMixer from './NatureMixer'
 import './styles.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <NatureMixer />
     <AnimalCalm />
   </StrictMode>,
 )
